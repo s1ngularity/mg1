8ed62f78adf0f11bbfaa0e43a1116b93211ea329
Alex Vaskov <avask@iMac-Alex.local>
2017-09-29 20:25:29 +0300

    Initial Commit

---
+++ mg1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mg1
+//
+//  Created by Alex Vaskov on 29.09.17.
+//  Copyright © 2017 Alex Vaskov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
